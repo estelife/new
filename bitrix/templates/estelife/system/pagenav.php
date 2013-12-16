da5ae70ec76637b34a3cd1d5f9f02ada1e264efb
Dmitriy Konev <dnkonev@yandex.ru>
2013-12-16 12:24:41 +0400

    Доработка постранички

--- bitrix/templates/estelife/system/pagenav.php
+++ bitrix/templates/estelife/system/pagenav.php
@@ -1,6 +1,12 @@
 <ul class="nav">
 	<?php
 	if(!empty($nEndPage)):
+		if($nStartPage>1):?>
+			<li><a href="<?=$sUrlPath?>?PAGEN_<?=$this->NavNum?>=1<?=$strNavQueryString?>">1</a></li>
+			<li><span>...</span></li>
+		<?php
+		endif;
+
 		$NavRecordGroup = $nStartPage;
 		while($NavRecordGroup <= $nEndPage)
 		{
@@ -11,6 +17,12 @@
 			<?php endif;
 			$NavRecordGroup++;
 		}
+
+		if($nEndPage<$this->NavPageCount):?>
+			<li><span>...</span></li>
+			<li><a href="<?=$sUrlPath?>?PAGEN_<?=$this->NavNum?>=<?=$this->NavPageCount.$strNavQueryString?>"><?=$this->NavPageCount?></a></li>
+		<?php
+		endif;
 	else:?>
 			<li><b>1</b></li>
 	<?php endif; ?>
