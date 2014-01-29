0100f8e66a617a949169fef078f66528b0755f3c
vitaly panait <panait.v@yandex.ru>
2014-01-29 11:40:48 +0400

    костыль для специалистов

--- bitrix/components/estelife/clinics.detail/templates/.default/template.php
+++ bitrix/components/estelife/clinics.detail/templates/.default/template.php
@@ -40,6 +40,9 @@
 						<?php if (!empty($arResult['clinic']["articles"])):?>
 							<li class="t4"><a href="#">Статьи<i></i></a></li>
 						<?php endif?>
+						<?php if ($arResult['clinic']["id"]==2):?>
+							<li class="t6"><a href="#">Специалисты<i></i></a></li>
+						<?php endif?>
 						<li class="t5"><a href="#">Контакты<i></i></a></li>
 					</ul>
 				</div>
@@ -143,26 +146,62 @@
 						</div>
 					</div>
 				</div>
-				<div class="tabs tab4 none">
-					<?php if (!empty($arResult['clinic']['articles'])):?>
+				<?php if (!empty($arResult['clinic']['articles'])):?>
+					<div class="tabs tab4 none">
 						<div class="items ">
-						<?php foreach ($arResult['clinic']['articles'] as $val):?>
+							<?php foreach ($arResult['clinic']['articles'] as $val):?>
 
-								<div class="item article">
-									<img src="<?=$val['img']?>" alt="<?=$val['name']?>" title="<?=$val['name']?>">
-									<h3><a href="<?=$val['url']?>"><?=$val['name']?></a></h3>
-									<p><?=$val['preview']?></p>
-									<ul class="stat">
-										<li class="date"><?=$val['date']?></li>
-										<li class="likes"><?=$val['countLike']?><i></i></li>
-										<li class="unlikes"><?=$val['countDislike']?><i></i></li>
-									</ul>
-								</div>
+									<div class="item article">
+										<img src="<?=$val['img']?>" alt="<?=$val['name']?>" title="<?=$val['name']?>">
+										<h3><a href="<?=$val['url']?>"><?=$val['name']?></a></h3>
+										<p><?=$val['preview']?></p>
+										<ul class="stat">
+											<li class="date"><?=$val['date']?></li>
+											<li class="likes"><?=$val['countLike']?><i></i></li>
+											<li class="unlikes"><?=$val['countDislike']?><i></i></li>
+										</ul>
+									</div>
 
-						<?php endforeach?>
+							<?php endforeach?>
 						</div>
-					<?php endif?>
-				</div>
+					</div>
+				<?php endif?>
+				<?php if ($arResult['clinic']['id']==2):?>
+					<div class="tabs tab6 none">
+						<div class="items ">
+							<div class="item article specialist">
+								<img src="/bitrix/templates/estelife/images/spec/rib.jpg" alt="Рыбакин Артур Владимирович" title="Рыбакин Артур Владимирович">
+								<h3>Рыбакин Артур Владимирович</h3>
+								<p>Пластический хирург, главный врач Института красоты СПИК, заведующий отделением эстетической пластической хирургии Института красоты СПИК</p>
+							</div>
+							<div class="item article specialist">
+								<img src="/bitrix/templates/estelife/images/spec/and.jpg" alt="Андреищев Андрей Русланович" title="Андреищев Андрей Русланович">
+								<h3>Андреищев Андрей Русланович</h3>
+								<p>Пластический и челюстно-лицевой хирург, ортодонт</p>
+							</div>
+							<div class="item article specialist">
+								<img src="/bitrix/templates/estelife/images/spec/arb.jpg" alt="Арбатов Вячеслав Витальевич" title="Арбатов Вячеслав Витальевич">
+								<h3>Арбатов Вячеслав Витальевич</h3>
+								<p>Пластический хирург</p>
+							</div>
+							<div class="item article specialist">
+								<img src="/bitrix/templates/estelife/images/spec/sok.jpg" alt="Соколов Григорий Никитич" title="Соколов Григорий Никитич">
+								<h3>Соколов Григорий Никитич</h3>
+								<p>Дерматолог-онколог, зав.отделением лазерной косметологии, к.м.н</p>
+							</div>
+							<div class="item article specialist">
+								<img src="/bitrix/templates/estelife/images/spec/bag.jpg" alt="Багненко Елена Сергеевна" title="Багненко Елена Сергеевна">
+								<h3>Багненко Елена Сергеевна</h3>
+								<p>Врач дерматолог-косметолог, трихолог, к.м.н</p>
+							</div>
+							<div class="item article specialist">
+								<img src="/bitrix/templates/estelife/images/spec/gin.jpg" alt="Гинтовт Елизавета Алексеевна" title="Гинтовт Елизавета Алексеевна">
+								<h3>Гинтовт Елизавета Алексеевна</h3>
+								<p>Врач дерматолог-косметолог, к.м.н</p>
+							</div>
+						</div>
+					</div>
+				<?php endif?>
 				<div class="tabs tab-c tab5 none">
 					<?php if (!empty($arResult['clinic']['contacts'])):?>
 						<?php foreach ($arResult['clinic']['contacts'] as $val):?>
