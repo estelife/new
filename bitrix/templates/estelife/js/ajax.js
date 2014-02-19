f91fbad72b42ffc75129ecbe0de62c5685ef2e54
Дмитрий Конев <dnkonev@yandex.ru>
2014-02-19 10:24:58 +0400

    правка по истории

--- bitrix/templates/estelife/js/ajax.js
+++ bitrix/templates/estelife/js/ajax.js
@@ -53,11 +53,24 @@ require([
 
 		EL.notice();
 
-		Backbone.history.start({
-			'pushState':true,
-			'hashChange': true,
-			'silent': true
-		});
+		if(Backbone.history && !Backbone.History.started) {
+			var historySettings;
+
+			if(!(window.history && history.pushState)) {
+				historySettings={
+					'pushState':false,
+					'hashChange': false,
+					'silent': true
+				};
+			}else {
+				historySettings={
+					'pushState':true,
+					'hashChange': true,
+					'silent': true
+				};
+			}
+			Backbone.history.start(historySettings);
+		}
 
 		//подстановка в url авторизации backurl
 		body.on('click', '.goto-auth', function(e){
